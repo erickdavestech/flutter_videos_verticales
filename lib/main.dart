2fc043e3f0f9deb9fc1f1a94225734c813f8bded
erickdavestech <erickdavestech@gmail.com>
2024-05-02 10:17:17 -0400

    dark_theme_main_working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'config/app_theme.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppTheme().getTheme(),
       title: 'CLone Toktik App',
       home: Scaffold(
         appBar: AppBar(
