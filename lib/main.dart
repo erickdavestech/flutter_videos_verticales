2b8edd6253cb05816a04926e4682d224191d2ef9
erickdavestech <erickdavestech@gmail.com>
2024-05-02 10:18:09 -0400

    remove_debug_banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
       title: 'CLone Toktik App',
       home: Scaffold(
