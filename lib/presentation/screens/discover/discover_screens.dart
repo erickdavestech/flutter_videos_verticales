04eafb0716674105836d202e08cd6dfda8805b36
erickdavestech <erickdavestech@gmail.com>
2024-05-02 14:02:42 -0400

    CircularProgressIndicator_+_placeholder

--- lib/presentation/screens/discover/discover_screens.dart
+++ lib/presentation/screens/discover/discover_screens.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_videos_verticales/presentation/providers_logic/discover_providers.dart';
+import 'package:provider/provider.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Discover Screen'),
-      ),
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    return Scaffold(
+      body: discoverProvider.initialLoading
+          ? const Center(child: CircularProgressIndicator())
+          : const Placeholder(),
     );
   }
 }
