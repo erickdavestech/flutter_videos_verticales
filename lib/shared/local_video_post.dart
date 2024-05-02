228df0e31e06110df3dab7ee73fa7208a8db4ea6
erickdavestech <erickdavestech@gmail.com>
2024-05-02 10:34:35 -0400

    shared_local_video_mapString_dynamic

---
+++ lib/shared/local_video_post.dart
@@ -0,0 +1,14 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  }
+];
